c7e84bd66fbfaba9fbc023a4308f07e2d2679e0e
vtduarte07 <vtduarte2007@gmail.com>
2026-04-25 23:09:36 -0300

    .gitignore

---
+++ src/main/java/com/vtduarte/pizzahub/PizzahubApplication.java
@@ -0,0 +1,13 @@
+package com.vtduarte.pizzahub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzahubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PizzahubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vtduarte/pizzahub/PizzahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vtduarte.pizzahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzahubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
